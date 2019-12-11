dc92b7bb33f45d4432fe3bd2b113f6d0a27255d8
Aleksandr Khozyainov <hozalex@gmail.com>
2019-12-11 22:24:07 +0300

    home work 3

--- lesson_3.py
+++ lesson_3.py
@@ -12,8 +12,16 @@ print(division(int(input("input first number ")), int(input("input second number
 
 # 2
 def user_description(name, lastname, birthday, city, email, phone):
-    print(f'{name} {lastname} {birthday} {city} {email} {phone}')
+    print(f'{name} {lastname}, birthday {birthday}, address - {city} {email} {phone}')
 
 
 user_description(name="John", lastname="Second", birthday="13.12.11", email="john@email", city="Gotham City",
                  phone="+9876554")
+
+
+# 3
+def my_func(x, y, z):
+    return sum([x, y, z]) - min([x, y, z])
+
+
+print(my_func(int(input("x = ")), int(input("y = ")), int(input("z = "))))

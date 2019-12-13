6ca0e6f86c8db884b7184dd02f8abc56339c2862
Aleksandr Khozyainov <hozalex@gmail.com>
2019-12-13 15:05:10 +0300

    home work

--- lesson_3.py
+++ lesson_3.py
@@ -9,8 +9,10 @@ def division(val1, val2):
 
 print(division(int(input("input first number ")), int(input("input second number "))))
 
-
 # 2
+print('_____________________________________')
+
+
 def user_description(name, lastname, birthday, city, email, phone):
     print(f'{name} {lastname}, birthday {birthday}, address - {city} {email} {phone}')
 
@@ -18,10 +20,70 @@ def user_description(name, lastname, birthday, city, email, phone):
 user_description(name="John", lastname="Second", birthday="13.12.11", email="john@email", city="Gotham City",
                  phone="+9876554")
 
-
 # 3
+print('_____________________________________')
+
+
 def my_func(x, y, z):
     return sum([x, y, z]) - min([x, y, z])
 
 
 print(my_func(int(input("x = ")), int(input("y = ")), int(input("z = "))))
+
+# 4
+print('_____________________________________')
+
+
+def exponentiation(x, y):
+    if y < 0 < x:
+        i = -1
+        res = x
+        while i > y:
+            res = res * x
+            y += 1
+        return 1 / res
+    else:
+        print("input positive and negative number")
+        return 0
+
+
+print(exponentiation(int(input("input positive number ")), int(input("input negative number "))))
+
+# 5
+print('_____________________________________')
+
+
+def number_function():
+    res = 0
+    print('input q for exit')
+    while True:
+        user_list = list(input('input you string ').split(' '))
+        for i in user_list:
+            if i.isdigit():
+                res += int(i)
+            elif i == 'q':
+                print('exit program')
+                return res
+        print(res)
+
+
+print(number_function())
+
+# 6
+print('_____________________________________')
+
+
+def int_func(string):
+    return str(string.lower()).title()
+
+
+def big_letter(user_str):
+    new_list = list()
+    for i in list(user_str.split(' ')):
+        new_list.append(int_func(i))
+
+    new_string = ' '.join(new_list)
+    return new_string
+
+
+print(big_letter(input('input you string ')))

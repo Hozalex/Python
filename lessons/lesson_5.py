0e2f37d869cb8919686a5aeed7f7594f545358bd
Aleksandr Khozyainov <hozalex@gmail.com>
2019-12-21 18:40:42 +0300

    refactor

--- lessons/lesson_5.py
+++ lessons/lesson_5.py
@@ -12,7 +12,7 @@ print("___________________ \n  #2")
 # 2
 count_string = 0
 count_words_in_string = 0
-with open("example2.txt", "r", encoding="utf8") as file2:
+with open("../files/example2.txt", "r", encoding="utf8") as file2:
     for string2 in file2.readlines():
         count_string += 1
         s2 = string2.split()
@@ -24,7 +24,7 @@ print("___________________ \n  #3")
 loser_list = []
 average_salary = 0
 count_lines = 0
-with open("example3.txt", "r", encoding="utf8") as file3:
+with open("../files/example3.txt", "r", encoding="utf8") as file3:
     for string3 in file3.readlines():
         s3 = string3.split()
         if int(s3[1]) < 20:
@@ -38,12 +38,12 @@ print("___________________ \n  #4")
 # 4
 str_count = 0
 ru_set = ["Один", "Два", "Три", "Четыре"]
-with open("example4.txt", "r", encoding="utf8") as file4:
+with open("../files/example4.txt", "r", encoding="utf8") as file4:
     for string4 in file4.readlines():
         s4 = string4.split()
         s4[0] = ru_set[str_count]
         str_count += 1
-        with open("new_example4.txt", "a", encoding="utf8") as n_file:
+        with open("../files/new_example4.txt", "a", encoding="utf8") as n_file:
             n_file.writelines("%s\n" % ' '.join(s4))
 print("___________________ \n  #5")
 
@@ -52,10 +52,10 @@ summary = 0
 number_list = []
 for i in range(10, 30):
     number_list.append(str(i))
-file5 = open("example5.txt", "w")
+file5 = open("../files/example5.txt", "w")
 file5.write(' '.join(number_list))
 file5.close()
-with open("example5.txt", "r") as file5:
+with open("../files/example5.txt", "r") as file5:
     sum_string = file5.read().split()
     for x in sum_string:
         summary += int(x)
@@ -65,7 +65,7 @@ print("___________________ \n  #6")
 # 6
 lesson_dict = {}
 lesson_hour = 0
-with open("example6.txt", "r", encoding="utf8") as file6:
+with open("../files/example6.txt", "r", encoding="utf8") as file6:
     for line in file6.readlines():
         k, *v = line.split()
         lesson_dict[k] = v

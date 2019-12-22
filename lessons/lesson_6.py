3d2c1a91c64ebfc46415f3c8f45e33350d365703
Aleksandr Khozyainov <hozalex@gmail.com>
2019-12-22 12:02:25 +0300

    home work

--- lessons/lesson_6.py
+++ lessons/lesson_6.py
@@ -1,8 +1,150 @@
-#1
+import time
+from functools import reduce
+
+
+# 1
 class TrafficLight:
     __color = None
 
     def running(self):
         while True:
+            TrafficLight.__color = 'Red'
+            print(TrafficLight.__color)
+            time.sleep(7)
+            TrafficLight.__color = 'Yellow'
+            print(TrafficLight.__color)
+            time.sleep(2)
+            TrafficLight.__color = 'Green'
+            print(TrafficLight.__color)
+            time.sleep(5)
 
-            
+
+# light = TrafficLight()
+# light.running()
+
+
+# 2
+print("2 _______________________")
+
+
+class Road:
+    __length = 0
+    __width = 0
+
+    def __init__(self, length, width):
+        Road.__length = length
+        Road.__width = width
+
+    def weight(self):
+        return (Road.__length * Road.__width * 25 * 5) // 1000
+
+
+road = Road(20, 5000)
+print(road.weight())
+
+# 3
+print("3 _______________________")
+
+
+class Worker:
+    name = ''
+    surname = ''
+    position = ''
+    __income = {'wage': 0, 'bonus': 0}
+
+
+class Position(Worker):
+    def __init__(self, name, surname, position, wage, bonus):
+        Worker.name = name
+        Worker.surname = surname
+        Worker.position = position
+        self._Worker__income['wage'] = wage
+        self._Worker__income['bonus'] = bonus
+
+    def get_full_name(self):
+        return f'Full Name = {Worker.name} {Worker.surname}'
+
+    def get_total_income(self):
+        return f'Total income = {reduce(lambda x, y: x + y, (x for x in self._Worker__income.values()))}'
+
+
+boss = Position('Ivan', 'Ivanov', 'boss', 100, 30)
+print(boss.get_full_name())
+print(boss.get_total_income())
+
+# 4
+print("4 _______________________")
+
+
+class Car:
+    speed = 0
+    color = ''
+    name = ''
+    is_police = None
+
+    def __init__(self, name, color, speed, is_police=False):
+        Car.name = name
+        Car.color = color
+        Car.speed = speed
+        Car.is_police = is_police
+
+    def go(self):
+        print(f'{self.name} has go')
+
+    def stop(self):
+        print(f'{self.name} has stopped')
+
+    def turn(self, direction):
+        print(f'{self.name} has turned {direction}')
+
+    def show_speed(self):
+        print(f'your speed {self.speed}')
+
+
+class TownCar(Car):
+
+    def show_speed(self):
+        if self.speed > 60:
+            print('your speed is over limit 60')
+        else:
+            Car.show_speed(self)
+
+
+class WorkCar(Car):
+
+    def show_speed(self):
+        if self.speed > 40:
+            print('your speed is over limit 40')
+        else:
+            Car.show_speed(self)
+
+
+class SportCar(Car):
+    pass
+
+
+class PoliceCar(Car):
+    Car.is_police = True
+
+
+work_car = WorkCar('work_lada', 'white', 50)
+work_car.go()
+work_car.show_speed()
+
+town_car = WorkCar('town_lada', 'green', 30)
+town_car.go()
+town_car.show_speed()
+town_car.stop()
+
+sport_car = SportCar('sport_lada', 'red', 80)
+sport_car.go()
+print(f'is this car police? {sport_car.is_police}')
+sport_car.turn("left")
+sport_car.show_speed()
+
+police_car = PoliceCar('police', 'white-blue', 70, True)
+police_car.go()
+print(f'is this car police? {police_car.is_police}')
+police_car.turn('right')
+police_car.show_speed()
+police_car.stop()
